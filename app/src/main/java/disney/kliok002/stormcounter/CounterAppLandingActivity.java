7f93b0e1cf3c4f60ecf80da32daf4eca8e0c7ddf
KLIOK002 <Kevin.H.Klions@disney.com>
2017-08-01 18:10:39 -0700

    landing screen now launches separate activities

--- app/src/main/java/disney/kliok002/stormcounter/CounterAppLandingActivity.java
+++ app/src/main/java/disney/kliok002/stormcounter/CounterAppLandingActivity.java
@@ -1,7 +1,10 @@
 package disney.kliok002.stormcounter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by kliok002 on 8/1/17.
@@ -13,6 +16,25 @@ public class CounterAppLandingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_screen);
 
+
+        Button launchLifeCounter = (Button) findViewById(R.id.launch_life_counter);
+        launchLifeCounter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CounterAppLandingActivity.this, LifeCounterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button launchStormCounter = (Button) findViewById(R.id.launch_storm_counter);
+        launchStormCounter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CounterAppLandingActivity.this, StormCounterActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 
 
