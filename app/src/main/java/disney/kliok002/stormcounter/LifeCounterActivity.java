14464874378ed98e5ad4df93076e5ae2e67b79a4
KLIOK002 <Kevin.H.Klions@disney.com>
2017-08-01 18:49:00 -0700

    added layout for life counter

--- app/src/main/java/disney/kliok002/stormcounter/LifeCounterActivity.java
+++ app/src/main/java/disney/kliok002/stormcounter/LifeCounterActivity.java
@@ -2,6 +2,8 @@ package disney.kliok002.stormcounter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by kliok002 on 8/1/17.
@@ -9,10 +11,25 @@ import android.support.v7.app.AppCompatActivity;
 
 public class LifeCounterActivity extends AppCompatActivity {
 
+    private TextView playerLifeTotalTextView;
+    private Button playerIncrementLifeTotalButton;
+    private Button playerDecrementLifeTotalButton;
+    private Button opponentIncrementLifeTotalButton;
+    private Button opponentDecrementLifeTotalButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_life_counter);
 
+        playerLifeTotalTextView = (TextView) findViewById(R.id.player_life_total);
+
+        playerIncrementLifeTotalButton = (Button) findViewById(R.id.player_life_increment);
+        playerDecrementLifeTotalButton = (Button) findViewById(R.id.player_life_decrement);
+
+        opponentIncrementLifeTotalButton = (Button) findViewById(R.id.player_life_increment);
+        opponentDecrementLifeTotalButton = (Button) findViewById(R.id.opponent_life_decrement);
 
     }
 }
